b97766cfd9c82350aa68eb4a5300d6f7ec836327
Nugroho Nur Cahyo <nugrohonug066@gmail.com>
2024-10-28 23:20:35 +0700

    Login page fixing + admin page new nav link, new user seeder

--- resources/views/components/welcome/hero.blade.php
+++ resources/views/components/welcome/hero.blade.php
@@ -1,20 +1,2 @@
-<div class="h-[100vh] w-full relative">
-    <div class="absolute inset-0 w-full h-[100dvh]">
-        {{-- content tengah --}}
-        <div class="md:translate-y-24 translate-y-36 h-80 w-full grid md:grid-cols-2 grid-cols-1 gap-12">
-            <div class="col-span-1 justify-center items-center md:grid hidden">
-                <img src="/images/ngaji.png" class="w-[24rem] h-[24rem] object-cover">
-            </div>
-            <div class="col-span-1 grid justify-center items-center">
-                <div class="text-primary md:w-[40rem] w-[18rem] h-full">
-                    <h1 class="md:text-4xl text-2xl font-bold my-10">Welcome to Islam Pro</h1>
-                    <p class="my-6 md:text-lg text-sm font-normal mb-10">Islam Pro adalah platform yang memudahkan umat Muslim membaca Al-Qur'an, mengakses jadwal kajian, mempelajari sunnah, dan menemukan doa sehari-hari. Dengan antarmuka sederhana, Islam Pro mendukung kemudahan dalam memperdalam pemahaman agama Islam.</p>
-                    <a href="#surah" class="md:p-4 p-2 border-2 border-primary text-primary  rounded-lg hover:bg-primary hover:text-white">
-                        baca quran
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <img src="/images/hero-wave.svg" alt="wave" class="bottom-0 absolute -z-10 object-cover h-[32rem] md:h-min">
-</div>
+
+
