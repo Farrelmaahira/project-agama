7f7e6c1a0a19693105450f999a25866aad0ead67
Nugroho Nur Cahyo <nugrohonug066@gmail.com>
2024-10-30 22:41:46 +0700

    tinymce config for textarea

---
+++ resources/views/components/head/tinymce-config.blade.php
@@ -0,0 +1,8 @@
+<script src="https://cdn.tiny.cloud/1/16rd4qe6jufnsbtdp6e1hgj81wv2pshf1j6twxe9wp3r3f6s/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
+<script>
+    tinymce.init({
+        selector: 'textarea#description', // Replace this CSS selector to match the placeholder element for TinyMCE
+        plugins: 'code table lists',
+        toolbar: 'undo redo | blocks | bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | code | table'
+    });
+</script>
