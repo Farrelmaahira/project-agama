119c734db0e2e69aac41e12f0e13f2d8899ecbf8
Nugroho Nur Cahyo <nugrohonug066@gmail.com>
2024-10-30 12:57:49 +0700

    fixing add kajian form

--- resources/views/admin/kajian/add-kajian.blade.php
+++ resources/views/admin/kajian/add-kajian.blade.php
@@ -6,19 +6,44 @@
             </h2>
         </div>
     </x-slot>
-    <div class="my-2 flex gap-4">
-        <label class="form-control w-full max-w-xs ">
+    <form class="my-2 grid md:grid-cols-2 grid-cols-1 gap-4" method="POST" action="{{route('admin.kajian.store')}}" enctype="multipart/form-data">
+        @method('POST')
+        @csrf
+        <label class="form-control w-full max-w-2xl md:col-span-1 col-span-2">
             <div class="label">
                 <span class="label-text">Judul Kajian</span>
             </div>
-            <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+            <input type="text" placeholder="Type here" name="judul" class="input input-bordered w-full max-w-2xl" />
         </label>
-        <label class="form-control w-full max-w-xs">
+        <label class="form-control w-full max-w-2xl md:col-span-1 col-span-2">
             <div class="label">
                 <span class="label-text">Masukkan Thumbnail</span>
             </div>
-            <input type="file" placeholder="Type here" class="file-input file-input-bordered w-full max-w-xs" />
+            <input type="file" placeholder="Type here" name="foto" class="file-input file-input-bordered w-full max-w-2xl" />
         </label>
+        <label class="form-control w-full max-w-2xl md:col-span-1 col-span-2">
+            <div class="label">
+                <span class="label-text">Tanggal Kajian</span>
+            </div>
+            <input type="date" placeholder="Type here" name="tanggal" class="input input-bordered w-full max-w-2xl" />
+        </label>
+        <label class="form-control w-full max-w-2xl md:col-span-1 col-span-2">
+            <div class="label">
+                <span class="label-text">Waktu Kajian</span>
+            </div>
+            <input type="time" placeholder="Type here" name="jam" class="file-input px-4 file-input-bordered w-full max-w-2xl" />
+        </label>
+        <label class="form-control w-full col-span-2">
+            <div class="label">
+                <span class="label-text">Kajian Description</span>
+            </div>
+            <textarea id="description" name="description"></textarea>
+        </label>
+        <div class="col-span-2 items-end">
+            <button class="btn btn-primary max-w-24 " type="submit">
+                Submit
+            </button>
+        </div>
+    </form>
 
-    </div>
 </x-layouts.admin-layout>
