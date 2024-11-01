dd0f6b8c8777d178051959f582aa35a08aff3559
Nugroho Nur Cahyo <nugrohonug066@gmail.com>
2024-11-01 22:18:13 +0700

    kajian & sunnah update v2

--- resources/views/user/kajian/kajian.blade.php
+++ resources/views/user/kajian/kajian.blade.php
@@ -1,20 +1,23 @@
 <x-layouts.app>
-    <div class="md:px-32 px-6 md:py-6 grid md:grid-cols-3 grid-cols-1 gap-4">
+    <div class="md:px-28 px-6 md:py-6 grid md:grid-cols-3 grid-cols-1 gap-4">
         @forelse($data as $d)
             <div class="bg-secondary md:p-4 rounded-md col-span-1 md:gap-4 gap-2 grid md:grid-cols-2 grid-cols-1">
                 <div class="col-span-1">
                     <img src="{{ asset('storage/' . $d->foto) }}" class="object-cover h-64 w-full rounded-md">
                 </div>
-                <div class="col-span-1 px-4">
-                    <h1 class="">
+                <div class="col-span-1 px-4 py-2 md:px-0 md:py-0 text-white ">
+                    <h1 class="line-clamp-1 hover:line-clamp-2 cursor-pointer mb-1  font-bold">
                         {{$d->judul}}
                     </h1>
-                    <h1>
-                        {{$d->tanggal}}
+                    <h1 class="mb-1">
+                        Tanggal: {{$d->tanggal}}
                     </h1>
-                    <h1>
-                        {{$d->jam}}
+                    <h1 class="mb-1">
+                        Jam: {{$d->jam}}
                     </h1>
+                    <div class="truncate line-clamp-4">
+                        {!! htmlspecialchars_decode($d->description) !!}
+                    </div>
                 </div>
             </div>
         @empty
--- resources/views/user/sunnah/sunnah.blade.php
+++ resources/views/user/sunnah/sunnah.blade.php
@@ -1,7 +1,7 @@
 <x-layouts.app>
     <div class="md:py-6 py-4 md:px-32 px-4 grid grid-cols-1 gap-4">
         @forelse($data as $d)
-            <div class="bg-secondary rounded-lg shadow-gray-200 p-8 grid  grid-cols-4 gap-8">
+            <div class="bg-secondary rounded-lg shadow-gray-200 p-4 grid  grid-cols-4 gap-8">
                 <div class="h-52 md:col-span-1 col-span-4">
                     <img src="{{ asset('storage/' . $d->foto) }}" class="object-cover h-52 w-full rounded-md">
                 </div>
